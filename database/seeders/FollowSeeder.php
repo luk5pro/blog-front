26277b402a869f5f908d4ccc5f2ec50aa8c58a56
lukas <lukakas.semper@gmail.com>
2024-06-03 01:48:45 +0300

    addedeverything

---
+++ database/seeders/FollowSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FollowSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = User::all();
+        foreach($users as $user){
+            $randUsers = $users->random(rand(0, $users->count()));
+            foreach($randUsers as $randUser){
+                if($randUser === $user){
+                    continue;
+                }
+                $user->followers()->attach($randUser);
+            }
+
+        }
+    }
+}
